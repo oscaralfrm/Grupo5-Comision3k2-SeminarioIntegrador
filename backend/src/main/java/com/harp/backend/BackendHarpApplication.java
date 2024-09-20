2f27ddbfdbad28afb39e95b6211b40e7aac56346
Oscar Romero <oscaralfrm1996@gmail.com>
2024-09-19 21:16:37 -0300

    Añadiendo esbozo del Backend con Springboot

---
+++ backend/src/main/java/com/harp/backend/BackendHarpApplication.java
@@ -0,0 +1,13 @@
+package com.harp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendHarpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendHarpApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/harp/backend/BackendHarpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendHarpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
