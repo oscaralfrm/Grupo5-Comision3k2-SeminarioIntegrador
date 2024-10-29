80426028c7adb271b37168e464c306cc1e4b1af9
Martin Spadachini <95168@sistemas.frc.utn.edu.ar>
2024-10-29 10:46:02 -0300

    Arreglos

--- harp/src/Components/LandingPage/CardInfo.jsx
+++ harp/src/Components/LandingPage/CardInfo.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import './LandingPage.css';
 import GenericCard from './GenericCard';
 import { TypeAnimation } from 'react-type-animation';
 import { FaMoneyBillWave, FaUserGraduate, FaBullhorn } from 'react-icons/fa';
 
 export default function CardInfo() {
   return (
-    <div style={{ marginTop: '11vw', marginBottom: '1rem' }}>
+    <div style={{ marginTop: '11vw', marginBottom: '1rem', width: '100%', overflowX: 'hidden' }}>
       <div className="row mb-4">
-        <h1 className="col-12 h1 text-left">
+        <h1 className='text-center'
+          style={{
+            color: '#1E1B4B',
+            textAlign: 'left',
+            fontFamily: 'Roboto, sans-serif',
+            fontSize: '3rem',
+            fontWeight: 'bold',
+          }}
+        >
           <TypeAnimation
             sequence={['Gestiona tus servicios con Harp', 1000]}
             speed={40}
@@ -18,32 +25,31 @@ export default function CardInfo() {
         </h1>
       </div>
 
-      <div className="row justify-content-center" style={{ marginLeft: '1vw' }}>
-        <div className="col-lg-4 col-md-6 col-sm-12 mb-4">
+      <div className="row justify-content-center" style={{ marginLeft: '1vw', display: 'flex' }}>
+        <div className="col-md-3 col-sm-12 mb-4" style={{ padding: '0 1vw' }}>
           <GenericCard
             title="Cobros Automatizados"
             description="Tus alumnos podrán abonar las cuotas por la aplicación de manera que queden registrados automáticamente."
             route="#"
-            icon={<FaMoneyBillWave />} // Icono representativo
-            color="#4F46E5" // Color de fondo
+            icon={<FaMoneyBillWave style={{ fontSize: '50px' }} />}
+            color="#4F46E5"
           />
         </div>
-        <div className="col-lg-4 col-md-6 col-sm-12 mb-4">
+        <div className="col-md-3 col-sm-12 mb-4" style={{ padding: '0 1vw' }}>
           <GenericCard
             title="Seguimiento de alumnos"
             description="Podrás registrar las asistencias de los alumnos en cada clase. También anotaciones y avances claves."
             route="#"
-            icon={<FaUserGraduate />}
-            // style = {{backgroundColor: 'linear-gradient(135deg, #1E1B4B, #4F46E5)'}}
-            color= "#4F46E5"
+            icon={<FaUserGraduate style={{ fontSize: '50px' }} />}
+            color="#4F46E5"
           />
         </div>
-        <div className="col-lg-4 col-md-6 col-sm-12 mb-4">
+        <div className="col-md-4 col-sm-12 mb-4" style={{ padding: '0 1vw' }}>
           <GenericCard
             title="Publicidad del servicio"
             description="Se les dará a conocer a personas interesadas tu servicio toda la información necesaria para que decidan inscribirse."
             route="#"
-            icon={<FaBullhorn />}
+            icon={<FaBullhorn style={{ fontSize: '50px' }} />}
             color="#4F46E5"
           />
         </div>
@@ -51,4 +57,3 @@ export default function CardInfo() {
     </div>
   );
 }
-
--- harp/src/Components/LandingPage/GenericCard.jsx
+++ harp/src/Components/LandingPage/GenericCard.jsx
@@ -3,14 +3,13 @@ import './GenericCard.css'; // Asegúrate de tener estilos para la tarjeta
 
 export default function GenericCard({ title, description, route, icon, color }) {
   return (
-    <div className="generic-card" style={{ backgroundColor: color }}>
-      <div className="icon-container">
-        {icon}
+    <div className="custom-card" style={{ backgroundColor: color }}>
+      <div className="icon-container d-flex justify-content-center align-items-center" style={{ height: '80px' }}>
+      {React.cloneElement(icon, { size: '2em' })}
       </div>
-      <h2>{title}</h2>
-      <p>{description}</p>
+      <h2 >{title}</h2>
+      <p >{description}</p>
       <a href={route} className="btn btn-primary">Más Información</a>
     </div>
   );
 }
-
