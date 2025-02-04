0a84615e06e255a72a1cab5e8485f99ab1977cf3
Julieta Buchaillot <julietab.utn@gmail.com>
2025-02-04 14:29:25 -0300

    Agregando casi nada

--- harp/src/services/frecuenciaPago.js
+++ harp/src/services/frecuenciaPago.js
@@ -31,11 +31,13 @@ export const armarStringPrecioYFrecuenciaCobro = (monto, cantCiclo, unidadCiclo)
     DAYS: { singular: "día", plural: "días", especial: "diario" },
   };
 
-  const { singular, plural, especial } = unidades[unidadCiclo];
+  if (unidadCiclo) {
+    const { singular, plural, especial } = unidades[unidadCiclo];
 
-  const frecuencia = cantCiclo === 1 ? especial : `cada ${cantCiclo} ${plural}`;
-
-  return `$${monto} ${frecuencia}`;
+    const frecuencia = cantCiclo === 1 ? especial : `cada ${cantCiclo} ${plural}`;
+  
+    return `$${monto} ${frecuencia}`;
+  }
 };
 
 
